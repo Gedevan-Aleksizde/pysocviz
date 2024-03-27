b8af90be037c8305288c31a89e54219310e46dee
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2024-03-28 02:58:14 +0900

    version specification

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='pysocviz',
-        version='0.2',
+        version='0.3.0',
         url='https://github.com/Gedevan-Aleksizde/pysocviz',
         license='',
         author='Katagiri, Satoshi',
         packages=find_packages(),
         install_requires=[
-            'adjustText >= 0.7.3',
+            'adjustText >= 1.1.1',
             'gapminder >= 0.1',
-            'geopandas >= 0.9.0',
-            'mizani >= 0.7.3',
-            'numpy >= 1.20.3',
-            'pandas >= 1.2.4',
+            'geopandas >= 0.14.3',
+            'mizani >= 0.11.0',
+            'numpy >= 1.26.4',
+            'pandas >= 2.2.1',
             'scikit-misc >= 0.1.4',
             'statsmodels >= 0.12.2',
-            'plotnine >= 0.8'
+            'plotnine >= 0.13.2'
         ],
         package_data={'pysocviz': ['data/*.csv', 'data/schema.json']},
         author_email='katagiri.stsh@gmail.com',
